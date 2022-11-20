70479a7c22c72bb3f7beabf4dc559a610fc50019
Unripe01 <tomato.ripe.dev@gmail.com>
2022-11-20 15:31:45 +0900

    first push

---
+++ lamp/htdocs/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <p>Hello World!!</p>
+    <?php
+      // phpinfo();
+      echo 'yunyun';
+    ?>
+  </body>
+</html>
